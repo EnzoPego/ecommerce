4bb0b1cdbeac89d23b85a426c97f9308d98f412b
EnzoPego <enzzo2410@gmail.com>
2023-02-27 00:43:50 -0300

    Funcionalidad del carrito terminada

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let articulosCarrito = []
 let total = 0
 let contenedorProductos = document.querySelector('.shop-items')
 let contenidoCarrito = document.querySelector('.cart-items')
-
+let precioFinal = document.querySelector('.cart-total-title')
 
 // Peticion al servidor
 async function obtenerDatos() {
@@ -56,24 +56,67 @@ function iterarProductos(productos) {
             let actualId = parseInt (evento.target.parentElement.parentElement.id)
             console.log(actualId)
 
-
             // Con el id encontrar el objeto actual
             let productoActual = productos.find(producto => producto.id == actualId)
-            console.log(productoActual)
+            
+            if(productoActual.quantity === undefined){
+                productoActual.quantity = 1
+            }
 
-            // Agregar el producto al arreglo articulosCarrito
-            contenidoCarrito.innerHTML += `
-            <div class="cart-row">
-                <div class="cart-item cart-column">
-                    <img class="cart-item-image" src=${productoActual.images[0]} >
-                    <span class="cart-item-title">${productoActual.title} </span>
-                </div>
-                <span class="cart-price cart-column">$${productoActual.price} </span>
-                <div class="cart-quantity cart-column">
-                    <input class="cart-quantity-input" min="1" type="number" value="1">
-                    <button class="btn btn-danger" type="button">REMOVE</button>
-            </div>`
+            // Pregunta si el producto que estoy agregando ya existe
+            
+            let existe = false
+            articulosCarrito.forEach(articulo =>{
+                if(actualId == articulo.id){
+                    existe = true
+                }
+            })
+
+            if (existe){
+                productoActual.quantity++
+                
+            }else{
+                // Pasa el producto seleccionado al arreglo articulosCarrito 
+                articulosCarrito.push(productoActual)
+            }
+
+            imprimirCarrito()
+  
+            // Actualizar el precio total del carrito
+            obtenerTotal() 
+                       
+            
         })
     })
 }
 
+function obtenerTotal(){
+    let sumTotal
+    let total = articulosCarrito.reduce((acc,item)=>{
+        sumTotal = acc + item.quantity*item.price
+        return sumTotal
+    },0)
+    // Imprime el total en el html
+    precioFinal.innerHTML = `$${total}`
+}
+
+
+function imprimirCarrito() {
+    // Limpia el HTML para no repetir los productos
+    contenidoCarrito.innerHTML = ``
+
+    articulosCarrito.forEach(item => {
+        contenidoCarrito.innerHTML += `
+            <div class="cart-row">
+                <div class="cart-item cart-column">
+                    <img class="cart-item-image" src=${item.images[0]} >
+                    <span class="cart-item-title">${item.title} </span>
+                </div>
+                <span class="cart-price cart-column">$${item.price} </span>
+                <div class="cart-quantity cart-column">
+                    <input class="cart-quantity-input" min="1" type="number" value="${item.quantity}">
+                    <button class="btn btn-danger" type="button">REMOVE</button>
+            </div>`
+    })
+
+}
